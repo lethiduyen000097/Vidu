e7aa1abaddc471f1eb54adcd110855ecbbd9ec01
lethiduyen000097 <duyenlt060097@gmail.com>
2021-03-30 01:17:10 -0700

    update

---
+++ tinh_tong_3_so_nguyen.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+	int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    printf("%d", a + b + c);
+}
+
+// Viet chuong trinh nhap vao tong 3 so nguyen a, b, c. Tính và in ra tong cua 3 so do
